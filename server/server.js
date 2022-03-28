3bfcd21269fee2d4735bb063c445cca2468fee05
Louie Mendez <lmendezjr024@gmail.com>
2022-03-28 13:32:21 -0400

    demo for stashql

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+// app.use(express.static(path.resolve(__dirname, '../dist')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('client'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server Listening on http://localhost:${PORT}`);
+})
